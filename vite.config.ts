51853d89528cee11fb3e51c1d1c8c0a5d0c32ad9
Tin TA <tinta@FGC4WRVXP4.local>
2026-06-14 12:54:29 +0700

    feat: allow host

--- vite.config.ts
+++ vite.config.ts
@@ -82,9 +82,15 @@ export default defineConfig({
     },
   ],
   server: {
-    host: "127.0.0.1",
+    host: "0.0.0.0",
     port: 5173,
     strictPort: true,
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: 5173,
+    strictPort: true,
+    allowedHosts: ["badminton.binbin-enterprise.com"],
   },
   resolve: {
     alias: {
